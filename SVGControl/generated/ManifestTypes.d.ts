fe0ac734ab21877f958f743e36d3fdb7c4ef6bdd
Luca Sain <32082555+lucahttp@users.noreply.github.com>
2023-11-28 14:22:43 -0300

    Add files via upload

---
+++ SVGControl/generated/ManifestTypes.d.ts
@@ -0,0 +1,14 @@
+/*
+*This is auto generated from the ControlManifest.Input.xml file
+*/
+
+// Define IInputs and IOutputs Type. They should match with ControlManifest.
+export interface IInputs {
+    svg: ComponentFramework.PropertyTypes.StringProperty;
+    zoomLevel: ComponentFramework.PropertyTypes.WholeNumberProperty;
+    zoomToId: ComponentFramework.PropertyTypes.StringProperty;
+    scale: ComponentFramework.PropertyTypes.WholeNumberProperty;
+    dataSet: ComponentFramework.PropertyTypes.DataSet;
+}
+export interface IOutputs {
+}
